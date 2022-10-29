6306b2d2cd631c314d1a196ee14dc67a86f6c840
OJddJO <95376647+OJddJO@users.noreply.github.com>
2022-10-29 22:44:23 +0200

    a

---
+++ replit.py
@@ -0,0 +1,40 @@
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QIcon
+from pypresence import Presence
+
+clientId = "1028218378357329990"
+try:
+    RPC = Presence(clientId)
+    RPC.connect()
+    print("Discord RPC is ready !")
+except:
+    print("Discord not found.")
+
+RPC.update(
+    large_image="logo",
+    large_text="Replit Desktop"
+)
+
+class WebEnginePage(QWebEnginePage):
+    def __init__(self, *args, **kwargs):
+        QWebEnginePage.__init__(self, *args, **kwargs)
+
+app = QApplication([])
+
+view = QWebEngineView()
+page = WebEnginePage()
+view.setPage(page)
+view.load(QUrl("https://replit.com/~"))
+
+view.setWindowTitle("Replit Desktop")
+view.setWindowIcon(QIcon('logo.png'))
+view.showMaximized()
+
+app.exec_()
+
+try:
+    RPC.close()
+except:
+    pass
